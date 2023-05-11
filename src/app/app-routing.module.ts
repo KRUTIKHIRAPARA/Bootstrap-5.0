ccc69634b17df7487e4e23e042448252526c1130
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-05-11 11:54:16 +0530

    Bootstrap component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccordionComponent } from './accordion/accordion.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { BadgesComponent } from './badges/badges.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { ButtonGroupComponent } from './button-group/button-group.component';
+import { CardComponent } from './card/card.component';
 
 const routes: Routes = [
   {
     path:'accordion',
     component:AccordionComponent
+  },
+  {
+    path:'alert',
+    component:AlertsComponent
+  },
+  {
+    path:'badges',
+    component:BadgesComponent
+  },
+  {
+    path:'breadcrumb',
+    component:BreadcrumbComponent
+  },
+  {
+    path:'buttons',
+    component:ButtonsComponent
+  },
+  {
+    path:'button-group',
+    component:ButtonGroupComponent
+  },
+  {
+    path:'card',
+    component:CardComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccordionComponent } from './accordion/accordion.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { BadgesComponent } from './badges/badges.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { ButtonGroupComponent } from './button-group/button-group.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccordionComponent
+    AccordionComponent,
+    AlertsComponent,
+    BadgesComponent,
+    BreadcrumbComponent,
+    ButtonsComponent,
+    ButtonGroupComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
