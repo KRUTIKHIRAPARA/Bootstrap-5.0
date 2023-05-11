8f8a2fa0abd6885f97db500dc8bfc009f120dc04
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-05-11 09:19:10 +0530

    accordion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccordionComponent } from './accordion/accordion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'accordion',
+    component:AccordionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
